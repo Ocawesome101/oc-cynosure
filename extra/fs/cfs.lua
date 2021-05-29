8c37a6f9a5f34e825a15f1ea5bd341ba285dcef6
ocawesome101 <ocawesome101@gmail.com>
2021-05-28 20:56:54 -0400

    more CFS things

--- extra/fs/cfs.lua
+++ extra/fs/cfs.lua
@@ -10,5 +10,20 @@ do
     uint64 "block_count",
     uint64 "inode_count",
     uint64 "fs_size",
+    uint16 "revision"
+  }
+  local inode = struct {
+    uint16 "permissions",
+    uint16 "owner",
+    uint16 "group",
+    uint16 "references",
+    uint64 "size",
+    uint64 "create",
+    uint64 "access",
+    uint64 "modify",
+    char[64] "padding",
+    char[40*8] "datablocks",
+    char[8*8] "indirect",
+    char[3*8] "double_indirect"
   }
 end
