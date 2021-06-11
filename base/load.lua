cba6d42ca3712888fcd009d9972d7eb222a90bfe
ocawesome101 <ocawesome101@gmail.com>
2021-06-11 18:00:49 -0400

    vastly improve forcible yielding - it should now be viable for general use, hopefully

--- base/load.lua
+++ base/load.lua
@@ -2,15 +2,17 @@
 
 k.log(k.loglevels.info, "base/load")
 
-if k.cmdline.force_yields then
+if (not k.cmdline.no_force_yields) then
   local patterns = {
-    { "if([ %(])(.-)([ %)])then([ \n])", "if%1%2%3then%4coroutine.yield(0)" },
-    { "while([ %(])(.-)([ %)])do([ \n])", "while%1%2%3do%4coroutine.yield(0) " },
-    { "for([ %(])(.-)([ %)])do([ \n])", "for%1%2%3do%4coroutine.yield(0) " },
-    { "repeat([ \n])", "repeat%1coroutine.yield(0) " },
+    { "if([ %(])(.-)([ %)])then([ \n])", "if%1%2%3then%4__internal_yield() " },
+    { "while([ %(])(.-)([ %)])do([ \n])", "while%1%2%3do%4__internal_yield() " },
+    { "for([ %(])(.-)([ %)])do([ \n])", "for%1%2%3do%4__internal_yield() " },
+    { "repeat([ \n])", "repeat%1__internal_yield() " },
   }
 
   local old_load = load
+
+  local max_time = tonumber(k.cmdline.max_process_time) or 0.5
 
   function _G.load(chunk, name, mode, env)
     checkArg(1, chunk, "function", "string")
@@ -30,6 +32,14 @@ if k.cmdline.force_yields then
 
     for i=1, #patterns, 1 do
       chunk = chunk:gsub(patterns[i][1], patterns[i][2])
+    end
+
+    local last_yield = computer.uptime()
+    env.__internal_yield = function()
+      if computer.uptime() - last_yield > max_time then
+        last_yield = computer.uptime()
+        coroutine.yield(0)
+      end
     end
 
     return old_load(chunk, name, mode, env)
