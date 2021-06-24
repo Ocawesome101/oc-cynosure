2fc57792e52f8679fa61b93ab86362cc677bdcf8
Ocawesome101 <ocawesome101@gmail.com>
2021-06-24 00:34:48 -0400

    release cynosure v1

--- base/version.lua
+++ base/version.lua
@@ -2,7 +2,7 @@
 
 do
   k._NAME = "Cynosure"
-  k._RELEASE = "0" -- not released yet
+  k._RELEASE = "1"
   k._VERSION = "@[{os.date('%Y.%m.%d')}]"
   _G._OSVERSION = string.format("%s r%s-%s", k._NAME, k._RELEASE, k._VERSION)
 end
