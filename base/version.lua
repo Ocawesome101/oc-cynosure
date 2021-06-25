7c0e8632c134a8cb5ee227672aad159bba16fc88
Ocawesome101 <ocawesome101@gmail.com>
2021-06-25 00:33:55 -0400

    bump release to 1.01

--- base/version.lua
+++ base/version.lua
@@ -2,7 +2,7 @@
 
 do
   k._NAME = "Cynosure"
-  k._RELEASE = "1"
+  k._RELEASE = "1.01"
   k._VERSION = "@[{os.date('%Y.%m.%d')}]"
   _G._OSVERSION = string.format("%s r%s-%s", k._NAME, k._RELEASE, k._VERSION)
 end
